83379d46bfcdb56fde90e690daf0d1c77a558b3c
VladislavYartsev <156785452+VladislavYartsev@users.noreply.github.com>
2026-06-08 14:44:28 +0500

    Final Changes

--- AIFastApi/App/app.py
+++ AIFastApi/App/app.py
@@ -7,9 +7,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:5000",  
-    "http://127.0.0.1:5000",
-    "*" 
+    "http://localhost:5001",
+    "http://127.0.0.1:5001"
 ]
 
 app.add_middleware(
--- AIFastApi/App/tasks.py
+++ AIFastApi/App/tasks.py
@@ -1,10 +1,16 @@
+import os
+
 from celery import Celery
+
 from App.model_service import get_predictor
+
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://taskflow-redis:6379/0")
 
 celery_app = Celery(
     "tasks",
-    broker="redis://taskflow-redis:6379/0", 
-    backend="redis://taskflow-redis:6379/0"
+    broker=REDIS_URL,
+    backend=REDIS_URL,
 )
 
 @celery_app.task
